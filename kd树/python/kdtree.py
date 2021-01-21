7003a9e402ce29a7321d8cc361ea3d25b3a0fafd
875977494 <875977494@qq.com>
2021-01-21 16:26:32 +0800

    create kdsearch

--- kd树/python/kdtree.py
+++ kd树/python/kdtree.py
@@ -1,50 +1,44 @@
-# 结点的数据结构
-class TreeNode(object) :
-    def __init__(self, data_self, split, left, right):
-        # 数据
-        self.data = data_self
-        # 第几个维度
+## create kdTree
+
+class TreeNode:
+    # data 数据
+    # split 划分域
+    # left right 左右孩子结点
+    def __init__(self, data, split, left, right) :
+        self.data = data
         self.split = split
-        # 孩子结点
         self.left = left
         self.right = right
 
-# 树的数据结构
-class KDTree(object):
-    def __init__(self):
+class KDTree:
+    def __init__(self, data):
         k = len(data[0])
-        def CreateNode(split, data_set) :
-            if not data_set:
-                return None
-            data_set.sort(key = lambda x : x[split])
-            split_pos = len(data_set) // 2
-            median = data_set[split_pos]
-            split_next = (split + 1) % k
 
+        def CreateNode(split, data_set) :
+            if not data_set : return None
+            data_set.sort(key = lambda  x : x[split])
+            split_pos = len(data_set) // 2
+            m = data_set[split_pos]
+            split_next = (split + 1) % k
             return TreeNode(
-                median,
+                m,
                 split,
                 CreateNode(split_next, data_set[:split_pos]),
                 CreateNode(split_next, data_set[split_pos + 1:])
             )
+
         self.root = CreateNode(0, data)
-def preorder(root) :
+
+def preorder(root):
     print(root.data)
     if root.left:
         preorder(root.left)
     if root.right:
         preorder(root.right)
 
-def inorder(root) :
-    if root.left:
-        inorder(root.left)
-    print(root.data)
-    if root.right:
-        inorder(root.right)
 
-def postorder(root) :
-    if root.left :
-        postorder(root.left)
-    if root.right :
-        postorder(root.right)
-    print(root.data)
+
+
+
+
+
